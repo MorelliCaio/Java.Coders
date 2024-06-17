e35c0fa0031121bdebd045d00fad65040e17dba0
Caio Morelli <morelliproducao@gmail.com>
2024-06-16 22:24:10 -0300

    add a new folders and files, and name of folder changed to 'introduçãoAoJava', for mor undertand

---
+++ introduçãoAoJava/Funções/Soma.java
@@ -0,0 +1,12 @@
+public class Soma {
+    
+    public static void main (String[] args){
+        int resultado = soma(2, 3);
+        resultado = soma(50, 30);
+        System.out.println(resultado);
+    }
+    
+    public static int soma(int a, int b){
+        return a + b;
+    }
+}
---
+++ introduçãoAoJava/Laços.de.repetição/Tabuada.java
@@ -0,0 +1,9 @@
+public class Tabuada {
+    public static void main (String[] args) {
+        for (int i = 1;  i <= 100; i++) {
+            for (int j = 1; j <= 10; j++){
+                System.out.println(j + "x" + i + "=" + j * i);
+            }
+        }
+    }
+}

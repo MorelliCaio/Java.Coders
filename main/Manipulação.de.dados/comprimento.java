31140862ab9976e58c74cb57a12dba752174ed6b
Caio Morelli <morelliproducao@gmail.com>
2024-06-16 22:04:52 -0300

    add a new folders and files for study, and anotations about programation logic

--- main/Manipulação.de.dados/comprimento.java
+++ main/Manipulação.de.dados/comprimento.java
@@ -3,7 +3,7 @@ import java.time.LocalDateTime;
 import java.time.format.TextStyle;
 import java.util.Locale;
 
-public class App {
+public class comprimento {
     public static void main(String[] args) throws Exception {
         
         String nome = "Caio";
@@ -13,7 +13,7 @@ public class App {
         String diaSemana = hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, Brasil);
         String saudacao;
         LocalDateTime agora = LocalDateTime.now();
-        if (agora.getHour() >= 0 && agora.getHour() < 11) {
+        if (agora.getHour() >= 6 && agora.getHour() < 11) {
             saudacao = "bom dia";
         } else if (agora.getHour() >= 12 && agora.getHour() < 18) {
             saudacao = "boa tarde";
